8fe8b973545ce0cf2486c2c79c03c77e0effd705
Paris Kostopoulos <pariskwsto@gmail.com>
2025-02-05 00:07:09 +0200

    fix(cmd): fix && operator in updateLinuxSystem

--- cmd/updateLinuxSystem.go
+++ cmd/updateLinuxSystem.go
@@ -29,7 +29,39 @@ var updateLinuxSystemCmd = &cobra.Command{
 
 // updateLinux executes the necessary commands to update the Linux system
 func updateLinux() error {
-	cmd := exec.Command("sudo", "apt", "update", "&&", "sudo", "apt", "upgrade", "-y", "&&", "sudo", "apt", "autoremove", "-y")
+	fmt.Println("Updating package lists...")
+	if err := aptUpdateCmd(); err != nil {
+		return err
+	}
+
+	fmt.Println("Upgrading installed packages...")
+	if err := aptUpgradeCmd(); err != nil {
+		return err
+	}
+
+	fmt.Println("Removing unnecessary packages...")
+	return aptAutoremoveCmd()
+}
+
+// aptUpdateCmd executes `sudo apt update`
+func aptUpdateCmd() error {
+	cmd := exec.Command("sudo", "apt", "update")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+// aptUpgradeCmd executes `sudo apt upgrade -y`
+func aptUpgradeCmd() error {
+	cmd := exec.Command("sudo", "apt", "upgrade", "-y")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+// aptAutoremoveCmd executes `sudo apt autoremove -y`
+func aptAutoremoveCmd() error {
+	cmd := exec.Command("sudo", "apt", "autoremove", "-y")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
